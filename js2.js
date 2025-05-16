0bf4451b09a5a67713bcdda29ec8e318dfbb5886
Eswar2585 <eswar20005@gmail.com>
2025-05-16 11:51:24 +0530

    first commit

---
+++ js2.js
@@ -0,0 +1,46 @@
+
+setTimeout(
+    ()=>{
+        document.getElementById("num").innerHTML=10
+        setTimeout(
+    ()=>{
+        document.getElementById("num").innerHTML=9
+       setTimeout(
+    ()=>{
+        document.getElementById("num").innerHTML=8
+        setTimeout(
+    ()=>{
+        document.getElementById("num").innerHTML=7
+        setTimeout(
+    ()=>{
+    document.getElementById("num").innerHTML=6
+        setTimeout(
+    ()=>{
+        document.getElementById("num").innerHTML=5
+        setTimeout(
+    ()=>{
+        document.getElementById("num").innerHTML=4
+        setTimeout(
+    ()=>{
+        document.getElementById("num").innerHTML=3
+      setTimeout(
+    ()=>{
+        document.getElementById("num").innerHTML=2
+        setTimeout(
+    ()=>{
+        document.getElementById("num").innerHTML=1
+        setTimeout(
+    ()=>{
+        document.getElementById("num").innerHTML="Happy new Year"
+        
+    },2000)
+    },2000)
+    },2000)  
+    },2000)
+    },1500)
+    },1500)
+    },2000)
+    },2000)
+    },2000) 
+    },2000)
+    },2000)
